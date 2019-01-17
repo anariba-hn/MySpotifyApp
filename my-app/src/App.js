b0921eb56a22f1091aacdc4c70e7fb9a867cf235
Dennis Anariba <anariba.hn@hotmail.com>
2019-01-17 12:40:14 -0600

    Load component with an acces token

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
+import queryString from 'query-string';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      apiData: {}
+    }
+  }
+
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.display_name
+      }
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" style={{width: 80, height: 80}}/>
-          <p>
-            My Spotify Suggestion Music Tool 
-          </p>
-          <button className="btn btn-success mt-3" onClick={()=> window.location='http://localhost:8888/login'}>
-            Log In with your account
-          </button>
-        </header>
-      </div>
+        { this.state.user ?
+            <div>     
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" style={{width: 80, height: 80}}/>
+                <h1> Welcome back {this.state.user.name} </h1>
+            </header>
+            </div>   
+            :
+            <div>     
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" style={{width: 80, height: 80}}/>
+                <p>
+                  My Spotify Suggestion Music Tool 
+                </p>
+                <button className="btn btn-success mt-3" onClick={()=> window.location='http://localhost:8888/login'}>
+                  Log In with your account
+                </button>
+              </header>
+            </div>    
+        } 
+        </div>
     );
   }
 }
